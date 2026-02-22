1957f5e3a3e0ccd7e311b6876fee6633ca0029b1
Daksh1111174 <shahdakshinfo@gmail.com>
2026-02-22 18:56:59 +0530

    Create lstm_model.py

---
+++ lstm_model.py
@@ -0,0 +1,28 @@
+# lstm_model.py
+
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+
+def train_lstm(data):
+
+    scaler = MinMaxScaler()
+    scaled_data = scaler.fit_transform(data.reshape(-1,1))
+
+    X, y = [], []
+    for i in range(60, len(scaled_data)):
+        X.append(scaled_data[i-60:i])
+        y.append(scaled_data[i])
+
+    X, y = np.array(X), np.array(y)
+
+    model = Sequential()
+    model.add(LSTM(50, return_sequences=True, input_shape=(X.shape[1],1)))
+    model.add(LSTM(50))
+    model.add(Dense(1))
+
+    model.compile(optimizer="adam", loss="mse")
+    model.fit(X, y, epochs=5, batch_size=32, verbose=0)
+
+    return model, scaler
