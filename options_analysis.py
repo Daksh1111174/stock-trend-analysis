370782f67c831f7e67f7948d419a423c8279933b
Daksh1111174 <shahdakshinfo@gmail.com>
2026-02-22 19:10:14 +0530

    Update options_analysis.py

--- options_analysis.py
+++ options_analysis.py
@@ -5,16 +5,21 @@ import pandas as pd
 
 def get_option_chain(ticker="^NSEI"):
 
-    stock = yf.Ticker(ticker)
+    try:
+        stock = yf.Ticker(ticker)
 
-    expiries = stock.options
-    if not expiries:
-        return None
+        expiries = stock.options
+        if not expiries:
+            return None, None, None
 
-    expiry = expiries[0]
-    opt = stock.option_chain(expiry)
+        expiry = expiries[0]
+        opt = stock.option_chain(expiry)
 
-    calls = opt.calls
-    puts = opt.puts
+        calls = opt.calls
+        puts = opt.puts
 
-    return calls, puts, expiry
+        return calls, puts, expiry
+
+    except Exception as e:
+        print("Option Chain Error:", e)
+        return None, None, None
